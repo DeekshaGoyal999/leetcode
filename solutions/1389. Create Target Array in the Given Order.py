35fdab63b957659c1480838bca5963f7ae99c319
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-05-27 20:10:58 +0530

    32 ms

---
+++ solutions/1389. Create Target Array in the Given Order.py
@@ -0,0 +1,6 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        l1=[]
+        for i in range(len(index)):
+           l1.insert(index[i],nums[i])  
+        return l1
