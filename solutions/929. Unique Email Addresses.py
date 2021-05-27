baa21636db04de2b709fcd03e9be242e45053ed6
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-05-28 01:13:51 +0530

    80 ms

---
+++ solutions/929. Unique Email Addresses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        '''m=[]
+        for i in range(len(emails)):
+            a=emails[i].split("@")
+            p=""
+            for j in range(len(a[0])):
+                if a[0][j]==".":
+                    continue
+                if a[0][j]=="+":
+                    break
+                p+=a[0][j]
+            p=p+"@"+a[1]
+            m.append(p)
+        return len(set(m))'''
+      
+        
+        
+        
+        
+        
+        
