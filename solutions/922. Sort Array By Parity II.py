378aec2eef30150957f97368fbad8a5d658026eb
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-05-27 20:13:00 +0530

    204 ms

---
+++ solutions/922. Sort Array By Parity II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        '''l=[]
+        m=[]
+        k=[]
+        for i in nums:
+            if i%2==0:
+                l.append(i)
+            else:
+                m.append(i)
+        for j in range(len(l)):
+            k.append(l[j])
+            k.append(m[j])
+        return k'''
+        
+        
+        
+        
+        
+        
+        
+        
