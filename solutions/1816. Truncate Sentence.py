42dc7ae9ffa863f21ad4128cf6d7996a91ff6d57
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-05-30 00:19:06 +0530

    28 ms

---
+++ solutions/1816. Truncate Sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        l= s.split(" ")
+        s=[]
+        i=0
+        while(k!=0):
+            s.append(l[i])
+            i+=1
+            k-=1
+        return (" ".join(s))
