06691d886543013998f3b9280dada0b35f159f17
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-06-21 01:30:32 +0530

    28 ms

--- solutions/367. Valid Perfect Square.py
+++ solutions/367. Valid Perfect Square.py
@@ -1,10 +1,29 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        n=num**0.5
-        m= math.ceil(n)
-        print(m)
-        if m == n :
-            return True
-        else:
-            return False
+        #Method-1
+        # n=num**0.5
+        # m= math.ceil(n)
+        # print(m)
+        # if m == n :
+        #     return True
+        # else:
+        #     return False
+        #Method-2
+        low=1
+        high=num
         
+        while low<=high:
+            mid=(low+high)//2
+            
+            if mid*mid==num:
+                return True
+            
+            elif mid*mid>num:
+                high=mid-1
+                
+            elif mid*mid<num:
+                low=mid+1
+                
+        return False
+            
+            
