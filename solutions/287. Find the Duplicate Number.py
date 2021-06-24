9f3e717cd36e1731f5cc7d39d6bc187c22a3d03b
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-06-24 15:46:00 +0530

    716 ms

--- solutions/287. Find the Duplicate Number.py
+++ solutions/287. Find the Duplicate Number.py
@@ -7,10 +7,32 @@ class Solution:
         #     return nums[i]
         # else:
         #     return self.findDuplicate((nums[(i+1):]))
-        n1=sorted(nums)
-        i=0
-        while(n1):
-            if n1[i]==n1[i+1]:
-                return n1[i]
+        #Method-2
+        # n1=sorted(nums)
+        # i=0
+        # while(n1):
+        #     if n1[i]==n1[i+1]:
+        #         return n1[i]
+        #     else:
+        #         i=i+1
+        # Method-3
+        nums.sort()
+        l=0
+        r=len(nums)-1
+        while(l<=r):
+            mid=(l+r)//2
+            if nums[mid]<mid+1:
+                r=mid-1
             else:
-                i=i+1
+                l=mid+1
+        return l
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
