6fe7b7883dae92cd5e6ea5bd162209bbb8b3c25d
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-06-24 18:53:12 +0530

    8612 ms

--- solutions/152. Maximum Product Subarray.py
+++ solutions/152. Maximum Product Subarray.py
@@ -1,18 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return nums[0]
-        else:
-            maxpro=0
-            m=max(nums)
-            for i in range(len(nums)-1):
-                pro=nums[i]
-                for j in range(i+1,len(nums)):
-                    pro*=nums[j]
-                    if pro > maxpro:
-                        maxpro=pro
-        if m >maxpro:
-            maxpro=m
+        maxpro=max(nums)
+        for i in range(len(nums)-1):
+            pro=nums[i]
+            for j in range(i+1,len(nums)):
+                pro*=nums[j]
+                if pro > maxpro:
+                    maxpro=pro
         return maxpro
         
         
