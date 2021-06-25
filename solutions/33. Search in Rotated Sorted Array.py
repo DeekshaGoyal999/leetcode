3b338abba35040d462577b291138f74e0e0cf5ed
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-06-25 16:37:34 +0530

    44 ms

---
+++ solutions/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target not in nums:
+            return -1
+        if target in nums:
+            return nums.index(target)
