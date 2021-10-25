f34eeddcee4e753f5c27ac935b414f73a4162f64
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-10-25 17:47:09 +0530

    40 ms

--- solutions/74. Search a 2D Matrix.py
+++ solutions/74. Search a 2D Matrix.py
@@ -1,40 +1,36 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # #Method-1
-        # for i in range(len(matrix)):
-        #     for j in range(len(matrix[0])):
-        #         if matrix[i][j]==target:
-        #             return True
-        #         else:
-        #             pass
-        # return False
-        # #Method-2
-        # for i in range(len(matrix)):
-        #         if matrix[i][0]<=target and matrix[i][-1]>=target:
-        #             if target in matrix[i]:
-        #                 return True
-        # return False
-        #Method-3
-        m=len(matrix)
-        n=len(matrix[0])
-        if target<matrix[0][0] or target> matrix[-1][-1]:
-            return False
-        else:
-            for i in range(m):
-                if matrix[i][0]<=target and matrix[i][-1]>=target:
-                    l=0
-                    r=len(matrix[0])
-                    while(l<=r):
-                        p=l+(r-l)//2
+#             return False
+#         else:
+#             for i in range(m):
+#                 if matrix[i][0]<=target and matrix[i][-1]>=target:
+#                     l=0
+#                     r=len(matrix[0])
+#                     while(l<=r):
+#                         p=l+(r-l)//2
               
-                        if matrix[i][p]==target:
-                            return True
-                        if matrix[i][p]>target:
-                            r=p-1
-                        if matrix[i][p]<target:
-                            l=p+1
-                    if target== matrix[i][p]:
-                        return True
+#                         if matrix[i][p]==target:
+#                             return True
+#                         if matrix[i][p]>target:
+#                             r=p-1
+#                         if matrix[i][p]<target:
+#                             l=p+1
+#                     if target== matrix[i][p]:
+#                         return True
+#         return False
+        #Method-4 
+        
+        m= len(matrix)
+        n=len(matrix[0])
+        l=0
+        r=(m*n)-1
+       
+        while(l<=r):
+            mid=l+(r-l)//2      
+            if matrix[mid//n][mid%n]==target:
+                return True
+            elif matrix[mid//n][mid%n]>target:
+                r=mid-1
+            else:
+                l=mid+1
         return False
                     
                     
@@ -45,4 +41,4 @@ class Solution:
                     
                     
                     
-                    
+        
