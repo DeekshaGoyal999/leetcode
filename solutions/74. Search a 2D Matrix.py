8a05c664a3bbd18821bd30c7dda70d0c71a0cbc6
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-10-25 16:46:52 +0530

    40 ms

--- solutions/74. Search a 2D Matrix.py
+++ solutions/74. Search a 2D Matrix.py
@@ -8,10 +8,32 @@ class Solution:
         #         else:
         #             pass
         # return False
-        #Method-2
-        for i in range(len(matrix)):
+        # #Method-2
+        # for i in range(len(matrix)):
+        #         if matrix[i][0]<=target and matrix[i][-1]>=target:
+        #             if target in matrix[i]:
+        #                 return True
+        # return False
+        #Method-3
+        m=len(matrix)
+        n=len(matrix[0])
+        if target<matrix[0][0] or target> matrix[-1][-1]:
+            return False
+        else:
+            for i in range(m):
                 if matrix[i][0]<=target and matrix[i][-1]>=target:
-                    if target in matrix[i]:
+                    l=0
+                    r=len(matrix[0])
+                    while(l<=r):
+                        p=l+(r-l)//2
+              
+                        if matrix[i][p]==target:
+                            return True
+                        if matrix[i][p]>target:
+                            r=p-1
+                        if matrix[i][p]<target:
+                            l=p+1
+                    if target== matrix[i][p]:
                         return True
         return False
                     
@@ -21,4 +43,6 @@ class Solution:
                     
                     
                     
-        
+                    
+                    
+                    
