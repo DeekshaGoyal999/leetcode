61d1d58813a584972c6e29c8d653ecc307281432
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-10-29 21:49:42 +0530

    136 ms

--- solutions/73. Set Matrix Zeroes.py
+++ solutions/73. Set Matrix Zeroes.py
@@ -11,14 +11,18 @@
                 if matrix[i][j]==0:
                     matrix[0][j]=0
                     matrix[i][0]=0
-        for k in range(1,n):
-            if matrix[0][k]==0:
-                for l in range(1,m):
-                    matrix[l][k]=0
-        for p in range(m):
-            if matrix[p][0]==0:
-                for y in range(1,n):
-                    matrix[p][y]=0
+        # for k in range(1,n):
+        #     if matrix[0][k]==0:
+        #         for l in range(1,m):
+        #             matrix[l][k]=0
+        # for p in range(m):
+        #     if matrix[p][0]==0:
+        #         for y in range(1,n):
+        #             matrix[p][y]=0
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][0]==0 or matrix[0][j]==0:
+                    matrix[i][j]=0
         if matrix[0][0]==0:
             for i in range(n):
                 matrix[0][i]=0
@@ -34,7 +38,3 @@
                     
                     
                     
-                    
-        
-        
-        
