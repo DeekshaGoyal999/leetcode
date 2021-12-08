7a034b7761e9c97d86a662f466170cf874a4bc65
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-12-08 10:59:10 +0530

    132 ms

--- solutions/73. Set Matrix Zeroes.py
+++ solutions/73. Set Matrix Zeroes.py
@@ -1,40 +1,36 @@
-        #Method-2
-        m=len(matrix)
-        n=len(matrix[0])
-        is_col= False
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
         
+        is_col=False
+        row=len(matrix)
+        col=len(matrix[0])
         
-        for i in range(m):
+        for i in range(row):
             if matrix[i][0]==0:
-                is_col= True
-            for j in range(1,n):
+                is_col=True
+            for j in range(1,col):
                 if matrix[i][j]==0:
-                    matrix[0][j]=0
                     matrix[i][0]=0
-        # for k in range(1,n):
-        #     if matrix[0][k]==0:
-        #         for l in range(1,m):
-        #             matrix[l][k]=0
-        # for p in range(m):
-        #     if matrix[p][0]==0:
-        #         for y in range(1,n):
-        #             matrix[p][y]=0
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][0]==0 or matrix[0][j]==0:
-                    matrix[i][j]=0
+                    matrix[0][j]=0
+        
+        for i in range(1,row):
+            for j in range(1,col):
+                if matrix[i][0] ==0 or matrix[0][j]==0:
+                        matrix[i][j]=0
+         
         if matrix[0][0]==0:
-            for i in range(n):
-                matrix[0][i]=0
-            
+            for j in range(col):
+                matrix[0][j]=0
+        
         if is_col:
-            for i in range(m):
+            for i in range(row):
                 matrix[i][0]=0
                 
-            
-                    
-        return matrix
-                    
-                    
-                    
+                
+                
+                
+        
                     
