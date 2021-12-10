0b774ee9fa92188dafe76ae70bb31b06c5e8c228
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2021-12-10 17:58:00 +0530

    48 ms

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -1,9 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range (0,len(nums)):
-            for j in range (i+1, len(nums)):
-                if (nums[i]+nums[j]== target):
-                    return(i,j)
+        # #Method-1
+        # for i in range (0,len(nums)):
+        #     for j in range (i+1, len(nums)):
+        #         if (nums[i]+nums[j]== target):
+        #             return(i,j)
+        #method-2 (using hashmap)
+        hmap= {}
+        # filling each number with it's index(count) in hmap
+        for i,n in enumerate(nums):
+            diff=target-n
+            #if difference is found in hmap return the index stored for that differnce and n
+            if diff in hmap:
+                return [hmap[diff],i]
+            hmap[n]=i
+        return
+         
     
        
             
