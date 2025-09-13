a003bb4ccac936596850646a5a240a83250e9394
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2025-09-13 18:12:21 +0530

    Time: 0 ms (100%), Space: 18 MB (17.32%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -1,12 +1,22 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
         #brute force method rotating array each time and checking if its sorted
-        sorted_list =  sorted(nums)
+        # sorted_list =  sorted(nums)
+        # l=len(nums)
+        # for k in range(len(nums)):
+        #     rotated_list = nums.copy()
+        #     for i in range(len(nums)):
+        #         rotated_list[(i+k)%l] =nums[i] 
+        #     if rotated_list == sorted_list:
+        #         return True
+        # return False
+        # optimised way as this break this only happen once in the loop so to check value once its just i+1 to check in loop %n and only once it will be possible 
+        rotation_error=0
         l=len(nums)
-        for k in range(len(nums)):
-            rotated_list = nums.copy()
-            for i in range(len(nums)):
-                rotated_list[(i+k)%l] =nums[i] 
-            if rotated_list == sorted_list:
-                return True
-        return False
+        for i in range(l):
+            if nums[i]>nums[(i+1)%l]:
+                rotation_error+=1
+            if rotation_error>1:
+                return False
+        return True
+        
