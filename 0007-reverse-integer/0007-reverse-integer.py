438f17411f934c661788eb07cb99ff436d5059b1
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2025-10-02 16:24:01 +0530

    Time: 49 ms (7.21%), Space: 17.7 MB (83.2%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,20 +1,37 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        #Method 1 bruute force converted each digit and concatenated it and typecasted it
+        # #Method 1 bruute force converted each digit and concatenated it and typecasted
+        # neg = False
+        # if x ==0:
+        #     return x
+        # if x <0:
+        #     neg= True
+        # a= abs(x)
+        # s=""
+        # while(a>0):
+        #     d= a%10
+        #     s+=str(d)
+        #     a=a//10
+        # s=int(s)
+        # if neg:
+        #     s= -1*s
+        # if s < -2**31 or s > 2**31 - 1:
+        #     return 0
+        # return s
+
+    # to avoid that string conversion
         neg = False
         if x ==0:
             return x
         if x <0:
             neg= True
         a= abs(x)
-        s=""
+        rev=0
         while(a>0):
-            d= a%10
-            s+=str(d)
-            a=a//10
-        s=int(s)
+            rev = rev * 10 + a % 10
+            a //= 10
         if neg:
-            s= -1*s
-        if s < -2**31 or s > 2**31 - 1:
+            rev= -1*rev
+        if rev < -2**31 or rev > 2**31 - 1:
             return 0
-        return s
+        return rev
