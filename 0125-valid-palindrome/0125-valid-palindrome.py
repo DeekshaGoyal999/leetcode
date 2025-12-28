0020463adb78c648e2dba243dd90a0e1e9f421c6
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2025-12-28 18:59:51 +0530

    Time: 3 ms (99.05%), Space: 18.9 MB (27.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,18 @@
 import re
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
+    def isPalindrome(self, s: str,i=0) -> bool:
         s= re.sub(r'[^A-Za-z0-9]','',s)
         s=s.lower()
+        # USing slicing
         if s == s[::-1]:
             return True
         return False
+        
+
+        # #using recursion
+        # n=len(s)
+        # if i>=n//2:
+        #     return True
+        # if s[i]!= s[n-i-1]:
+        #     return False
+        # return self.isPalindrome(s,i+1)
