d70b74cdd9ecd1dda5bfd25cb60d5a9ed658802c
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2026-05-14 00:12:35 +0530

    Time: 0 ms (100%), Space: 26.5 MB (44.59%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,17 +4,24 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        #M-1 Slicing
-        k = k % len(nums)
-        right_array = nums[-k:]
-        left_array = nums[:-k]
-        nums[:]= right_array + left_array
-        return nums
+        # #M-1 Slicing
+        # k = k % len(nums)
+        # right_array = nums[-k:]
+        # left_array = nums[:-k]
+        # nums[:]= right_array + left_array
+        # return nums
         # l=len(nums)
         # for i in range(len(nums)):
         #     # a=  nums[(i+k)%l]
         #     nums[(i+k)%l] = nums[i]
         #     # nums[i] =a
         # return nums
+
+        length = len(nums)
+        k= k% length
+        nums[:length-k]= reversed(nums[0:length-k])
+        nums[length-k:length] =reversed(nums[length-k:length])
+        return nums.reverse()
+       
 
         
