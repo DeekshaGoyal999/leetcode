66f3c64c9a05ae6fd7e20c1cf81859ec061b5d78
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2026-05-16 16:42:13 +0530

    Time: 3 ms (52.35%), Space: 20.4 MB (42.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-       # M-1 Brute Force
-       for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j]== target:
-                    return [i,j]
+    #    # brute force method - time O(n2), space- O(1) complexityy
+    #    for i in range(len(nums)):
+    #         for j in range(i+1,len(nums)):
+    #             if nums[i] + nums[j]== target:
+    #                 return [i,j]
         
+        # M-2 Dictionary
+        nums_dict = {}
+        for i in range(len(nums)):
+            find_element = target - nums[i]
+            if find_element in nums_dict:
+                return [i, nums_dict[find_element]]
+            else:
+                nums_dict[nums[i]]=i
