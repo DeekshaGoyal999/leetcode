932f80356896849e0dd2f23666c8782f6a41edfe
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2026-05-25 21:26:45 +0530

    Time: 17 ms (19.93%), Space: 19.4 MB (43.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!= len(t):
+        # # M-1 sorting overall time= O(nlogn), spcae =O(n) (used in creating that list)
+        # if len(s)!= len(t): # time= O(1)
+        #     return False
+        # if sorted(s) == sorted(t): #time for sorting = O(nlogn) and for array comparison= O(n)
+        #     return True
+        # else:
+        #     return False
+
+        #M-2 Hasmap time= O(n), space = O(n) - new hasmap created
+        
+        if len(s) != len(t):
             return False
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
+        count = {}
+
+        for char in s: #time= O(n)
+            count[char] = count.get(char,0)+1
+        
+        for char in t: #time= O(n)
+            if char not in count:
+                return False
+            count[char] -=1
+            if count[char]<0:
+                return False
+        return True
