8b8178467ba68afb2a5ed7ce0a0f7d23b8135eec
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2026-05-25 23:45:19 +0530

    Time: 7 ms (81.12%), Space: 19.7 MB (56.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,13 @@
 import re
 class Solution:
     def isPalindrome(self, s: str,i=0) -> bool:
-        s= re.sub(r'[^A-Za-z0-9]','',s)
-        s=s.lower()
-        # USing slicing
-        if s == s[::-1]:
-            return True
-        return False
+        # #M-1
+        # s= re.sub(r'[^A-Za-z0-9]','',s) # time - O(n) space- O(n) new string cretaed
+        # s=s.lower() #time - O(n) space- O(n) 
+        # # USing slicing
+        # if s == s[::-1]: #time - O(n) space- O(n) 
+        #     return True
+        # return False
         
 
         # #using recursion
@@ -16,3 +17,21 @@ class Solution:
         # if s[i]!= s[n-i-1]:
         #     return False
         # return self.isPalindrome(s,i+1)
+
+        #m-3 two pointer
+        left =0
+        right = len(s)-1
+        while right>left:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower() !=s[right].lower():
+                return False
+            left +=1
+            right-=1
+        return True
+    
+            
+            
+            
