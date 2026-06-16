3458c936a7b05439d4faee57d4ff61ef7b1874dd
Deeksha Goyal <45399767+DeekshaGoyal999@users.noreply.github.com>
2026-06-16 20:50:53 +0530

    Time: 11 ms (65.65%), Space: 32.2 MB (48.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,9 +1,24 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        #M-1 set
-        len_set_nums = len(set(nums))
-        if len_set_nums == len(nums):
-            return False
-        else:
-            return True
+        # #M-1 set
+        # len_set_nums = len(set(nums))
+        # if len_set_nums == len(nums):
+        #     return False
+        # else:
+        #     return True
+
+        # M-2 loop O(n) - not running lime limit exceeded
+        set_nums = set()
+        for i in nums:
+            if i in set_nums:
+                return True
+            set_nums.add(i)
+        return False
+
+        # #M-3
+        # for i in range(len(nums)):
+        #     if nums[i] in nums[i+1:]:
+        #         return True
+        # return False
+
         
